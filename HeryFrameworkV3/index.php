82b2401feb34cb403fdcd31fbc2d549afa609ff6
mrhery <aimanmrc@gmail.com>
2019-05-11 10:53:33 +0800

    update index

--- HeryFrameworkV3/index.php
+++ HeryFrameworkV3/index.php
@@ -1,8 +1,10 @@
 <?php
+/***********************************************
 ;"Hery PHP Framework (HPF) v3";
 ;"Designed and Developed at Min September 2018";
 ;"Intelligent Hosting Pte. Ltd. (1158583-U)";
 ;"Master Hery (iamhery.com)";
+************************************************/
 
 define("HFA", true);
 require_once(__DIR__ . "/Systems/init.php");
