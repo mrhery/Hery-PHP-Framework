7e7ffd1099c86202093e9344349d6b9ec0ebc434
Mr Hery <aimanmrc@gmail.com>
2019-07-29 23:35:36 +0800

    Update Input.php

--- HeryFrameworkV3/Systems/Core/Input.php
+++ HeryFrameworkV3/Systems/Core/Input.php
@@ -4,21 +4,28 @@ require_once(dirname(__DIR__) . "/Misc/document_access.php");
 class Input{
 	public function __construct(){}
 	
-	public static function get($name){
+	public static function get($name, $html = true){
 		if(isset($_GET[$name])){
-			return htmlspecialchars($_GET[$name]);
+			if($html){
+				return !is_array($_GET[$name]) ? htmlspecialchars($_GET[$name]) : $_GET[$name];
+			}else{
+				return !is_array($_GET[$name]) ? ($_GET[$name]) : $_GET[$name];
+			}
 		}else{
 			return "";
 		}
 	}
 	
-	public static function post($name){
+	public static function post($name, $html = true){
 		if(isset($_POST[$name])){
-			return htmlspecialchars($_POST[$name]);
+		    if($html){
+		        return !is_array($_POST[$name]) ? htmlspecialchars($_POST[$name]) : $_POST[$name];
+		    }else{
+		        return !is_array($_POST[$name]) ? ($_POST[$name]) : $_POST[$name];
+		    }
 		}else{
 			return "";
 		}
 	}
 }
-
 ?>
